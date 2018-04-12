ac5f6e15f79a0afede915f62e6646952920c23d5
Marco Enrico Piras <kikkomep@crs4.it>
2018-04-12 12:10:55 +0200

    Configure and use 'markdown2html' converter

--- bin/run.sh
+++ bin/run.sh
@@ -1,35 +1,19 @@
 #!/usr/bin/env bash
+
+# configuration
 path="/var/www/html/php-phenomenal-portal-wiki"
-markdownFolder="$path/wiki-markdown"
 htmlFolder="$path/wiki-html"
+markdownFolder="$path/wiki-markdown"
 gitList="$path/conf/gitList.txt"
-extension=".html"
+gitBranch="master"
 
-mkdir -p $markdownFolder
-mkdir -p $htmlFolder
+# path of this script
+current_path="$( cd "$(dirname "${0}")" ; pwd -P )"
 
-cd $markdownFolder && rm -rf *
-
-echo $gitList
-
-while IFS= read line
-do
-    git clone "$line"
-done <"$gitList"
-
-# For running inside cron, for markdown2
-PATH=/usr/local/bin/:$PATH
-
-for dir in `ls ./`;
-do
-    mkdir -p "$htmlFolder/$dir"
-    for file in `ls ./$dir`;
-    do
-	    if [[ -f "$dir/$file" ]]; then
-			filename="${file%.*}"
-			markdown2 --extras fenced-code-blocks "$dir/$file" > "$htmlFolder/$dir/$filename"
-			cp "$htmlFolder/$dir/$filename" "$htmlFolder/$dir/$filename$extension"
-	    fi
-	    cp -r "$dir/$file" "$htmlFolder/$dir/$file"
-    done
-done
+# launch converter
+"${current_path}/markdown2html/run.sh" \
+    --force-cleanup \
+    --html "${htmlFolder}" \
+    --md "${markdownFolder}" \
+    --git-branch "${gitBranch}" \
+    "${gitList}"
